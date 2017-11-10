fba7d456752d024cf6f107073c2d6c0882521472
bemenst <steve.bement@becu.org>
2017-11-10 13:20:03 -0800

    First test

---
+++ Money.Tests.Unit/MoneyTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+namespace MoneyExercise.Tests.Unit
+{
+    public class MoneyTests
+    {
+        [Test]
+        public void TestCase()
+        {
+            Money money2 = new Money(1.0m);
+            Money money1 = new Money(2.0m);
+
+            Money sum = money1.add(money2);
+
+            Assert.That(sum.Amount, Is.EqualTo(3.0m));
+        }
+    }
+}
---
+++ Money/Money.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MoneyExercise
+{
+    public class Money
+    {
+        private Decimal amount;
+
+        public Decimal Amount
+        {
+            get
+            {
+                return amount;
+            }
+        }
+
+        public Money(Decimal amount)
+        {
+            this.amount = amount;
+        }
+
+        public Money add(Money money2)
+        {
+            return new Money(3.0m);
+        }
+    }
+}
