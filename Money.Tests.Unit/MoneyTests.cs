366965feaca91c1c35b40aedc9bd17c26b756499
bemenst <steve.bement@becu.org>
2017-11-17 10:20:19 -0800

    Added mock.

--- Money.Tests.Unit/MoneyTests.cs
+++ Money.Tests.Unit/MoneyTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
+using Moq;
+
 namespace MoneyExercise.Tests.Unit
 {
     public class MoneyTests
@@ -31,12 +33,29 @@ namespace MoneyExercise.Tests.Unit
         }
 
         [Test]
-        public void ThreeUsdPlusFiveCad_ReturnsSumInCad()
+        public void ThreeUsdPlusFiveCad_ReturnsSumInCad_Stub()
         {
             Money money1 = new Money(3.0m, Currency.USD);
             Money money2 = new Money(5.0m, Currency.CAD);
 
             money1.SetExchangeService(new ExchangeServiceStub());
+            Money sum = money1.add(money2);
+
+            Money expectedResult = new Money(11m, Currency.CAD);
+
+            Assert.That(sum, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void ThreeUsdPlusFiveCad_ReturnsSumInCad_Mock()
+        {
+            Mock<CurrencyConverting> mockService = new Mock<CurrencyConverting>();
+            mockService.Setup(s => s.convert(3.0m, Currency.USD, Currency.CAD)).Returns(6.0m);
+
+            Money money1 = new Money(3.0m, Currency.USD);
+            Money money2 = new Money(5.0m, Currency.CAD);
+
+            money1.SetExchangeService(mockService.Object);
             Money sum = money1.add(money2);
 
             Money expectedResult = new Money(11m, Currency.CAD);
